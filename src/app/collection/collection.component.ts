b8434256e26fe03b47d1f9d81ad7d2d41435a815
Vuli Russell <vulirussell@gmail.com>
2020-11-18 22:12:28 +0000

    collection styling

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -14,7 +14,7 @@ export class CollectionComponent implements OnInit {
 
   ngOnInit(): void {
     this.collectionsService.booksStream.subscribe(
-      res => {this.books = res ? res.books.map(book => ({...book, isFav: true})) : []}
+      res => {this.books = res.books.length ? res.books.map(book => ({...book, isFav: true})) : null;}
     )
   }
 }
